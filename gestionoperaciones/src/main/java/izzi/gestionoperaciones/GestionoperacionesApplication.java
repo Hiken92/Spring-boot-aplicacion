e9c9b3dcaaac8f85d554ec55c52a1f5f506b0374
Hiken <Hiken@DESKTOP-UR6ABBQ>
2019-10-08 13:53:22 -0500

    Comit inicial con STS4 Close #1

---
+++ gestionoperaciones/src/main/java/izzi/gestionoperaciones/GestionoperacionesApplication.java
@@ -0,0 +1,13 @@
+package izzi.gestionoperaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionoperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionoperacionesApplication.class, args);
+	}
+
+}
---
+++ gestionoperaciones/src/test/java/izzi/gestionoperaciones/GestionoperacionesApplicationTests.java
@@ -0,0 +1,16 @@
+package izzi.gestionoperaciones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionoperacionesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
