c22e8aaf6e2ce421459ff019c12f81b9ed33bafc
Hiken <Hiken@DESKTOP-UR6ABBQ>
2019-10-14 13:12:59 -0500

    HTML commit

---
+++ gestionoperaciones/src/main/java/izzi/gestionoperaciones/controller/UserController.java
@@ -0,0 +1,13 @@
+package izzi.gestionoperaciones.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
